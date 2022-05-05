741a09729812420d0df02ab14643f81f0601a242
Aruva Ramakrishna Rao <Ramakrishna.Aruva@ivycomptech.com>
2022-05-05 19:47:20 +0530

    added homepage layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,19 @@
+import { Col, Row, Statistic, Typography } from 'antd'
 import React from 'react'
-
+ 
+const {Title} = Typography
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5 "/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value="5 "/></Col>
+        <Col span={12}><Statistic title="Total Market Cap " value="5 "/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value="5 "/></Col>
+        <Col span={12}><Statistic title="Total Markets" value="5 "/></Col>
+      </Row>
+    </>
   )
 }
 
